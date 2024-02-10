66564bb7fced60bd820fa461f139d9668d851141
Nuru1d33n <Nuruadebileje@gmail.com>
2024-02-10 10:47:52 +0000

    first commit

---
+++ static/assets/js/form.js
@@ -0,0 +1,12 @@
+function openForm() {
+    console.log('Clock');
+    document.getElementById("form-container").classList.add("show");
+    document.getElementById("form").classList.add("show");
+
+    // document.getElementById('body').classList
+}
+
+function closeForm() {
+    document.getElementById("form-container").classList.remove("show");
+    document.getElementById("form").classList.remove("show");
+}
